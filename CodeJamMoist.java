b272750439622cd39374612ed1faef6f10a01c30
Gary Bezet <Gary@GarysCorner.NET>
2016-06-14 09:28:31 -0500

    Initial commit

---
+++ CodeJamMoist.java
@@ -0,0 +1,21 @@
+package net.garyscorner.codejammoist;
+
+//File:		CodeJamMoist.java
+//Author:	Gary Bezet
+//Date:		2016-06-14
+//Desc:		Designed for google apac test "Practice Round APAC test 2016"  Written as practice
+//Problem:	https://code.google.com/codejam/contest/6234486/dashboard
+//Results:	
+
+public class CodeJamMoist {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
